cf74196b57e48b9b4fbb0bcd6502c0ae9bfed1fd
Jerem <jeremy.lechat72@gmail.com>
2015-01-17 11:28:18 +0100

    Jérémy est désormais contributrice(eur)

--- src/sopra/challenge/Main.java
+++ src/sopra/challenge/Main.java
@@ -21,10 +21,23 @@ along with <programm name>.  If not, see <http://www.gnu.org/licenses/>.
 */
 package sopra.challenge;
 
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Ready ?");
-	}
 
+		System.out.println("Hello java 8 world !");
+
+		List<String> strs = new LinkedList<String>(); strs.add("str1"); strs.add("str2");
+
+		strs.forEach(t -> System.out.println("lambda : " + t));
+
+		LocalDate date = LocalDate.now();
+
+		System.out.println(date);
+
+		}
 }
